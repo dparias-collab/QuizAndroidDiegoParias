bb401f40f8cf4ba75ca1570fb044e53555a255b3
Laboratorio de Telecomunicaciones <cisco@academico.unab.ad>
2026-02-27 09:50:42 -0500

    feat: primer paso

---
+++ app/src/main/java/com/diegoparias/quizandroid/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.diegoparias.quizandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.diegoparias.quizandroid.ui.theme.QuizAndroidDiegoPariasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+
+                }
+            }
+        }
+
+fun CompletedScreen(){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
